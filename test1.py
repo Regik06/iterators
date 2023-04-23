3b66a5a765522968b323a1431d816d46f124838e
Regik_06 <eparis@atlassian.com>
2023-04-23 16:40:43 +0900

    Initial commit

---
+++ test1.py
@@ -0,0 +1,47 @@
+class FlatIterator:
+
+    def __init__(self,list_of_list):
+        self.start = -1
+        self.end = len(list_of_list)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.start += 1
+        if self.start == self.end:
+            raise StopIteration
+        return self
+
+    def __str__(self):
+        return '\n'.join(str(flat) for flat in list_of_list[self.start])
+
+
+
+def test_1():
+
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            FlatIterator(list_of_list=list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(FlatIterator(list_of_list=list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+
+if __name__ == '__main__':
+    # test_1()
+    list_of_list = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+    for elem in FlatIterator(list_of_list=list_of_list):
+        print(elem)
