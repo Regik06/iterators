6edb39f80156177927e69cfaa8c4bee317cd78c1
Regik_06 <eparis@atlassian.com>
2023-05-14 17:19:58 +0900

    change

--- test1.py
+++ test1.py
@@ -1,21 +1,36 @@
 class FlatIterator:
 
-    def __init__(self,list_of_list):
-        self.start = -1
-        self.end = len(list_of_list)
+    def __init__(self, list_of_list):
+        self.lol = list_of_list
 
     def __iter__(self):
+        self.main = 0
+        self.sub = -1
+
         return self
 
     def __next__(self):
-        self.start += 1
-        if self.start == self.end:
+        if self.sub < len(self.lol[self.main]) - 1:
+            self.sub += 1
+        else:
+            self.main += 1
+            self.sub = 0
+        if self.main == len(self.lol):
             raise StopIteration
-        return self
+        item = self.lol[self.main][self.sub]
+        return item
 
-    def __str__(self):
-        return '\n'.join(str(flat) for flat in list_of_list[self.start])
 
+def flat_generator(list_of_lists):
+    main_counter = 0
+    sub_counter = 0
+    while main_counter < len(list_of_lists):
+        while sub_counter < len(list_of_lists[main_counter]):
+            res = list_of_lists[main_counter][sub_counter]
+            yield res
+            sub_counter += 1
+        sub_counter = 0
+        main_counter += 1
 
 
 def test_1():
@@ -37,11 +52,11 @@ def test_1():
 
 
 if __name__ == '__main__':
-    # test_1()
-    list_of_list = [
-        ['a', 'b', 'c'],
-        ['d', 'e', 'f', 'h', False],
-        [1, 2, None]
-    ]
-    for elem in FlatIterator(list_of_list=list_of_list):
-        print(elem)
+    test_1()
+    # list_of_list = [
+    #     ['a', 'b', 'c'],
+    #     ['d', 'e', 'f', 'h', False],
+    #     [1, 2, None]
+    # ]
+    # for elem in FlatIterator(list_of_list=list_of_list):
+    #     print(elem)
